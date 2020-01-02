31612b0620760256dbc04b10f11f4279180ee805
Amos Yu <amosyu2000@gmail.com>
2020-01-01 21:49:00 -0500

    can place pieces (VERY glitchy)

---
+++ blokkus/resources/js/blokkus.js
@@ -0,0 +1,228 @@
+// Class for piece 
+class Piece { 
+	constructor(rows, columns, anchor_row, anchor_column, value, grid){ // Takes 5 arguments (length, height, index_x, index_y, values)
+		this.rows = rows; // Row size of piece
+		this.columns = columns; // Column size of piece
+		this.anchor_point = [anchor_row, anchor_column]; // Rotational point of piece 
+		this.top = 1; // Top position
+		this.bottom = (rows - 2); // Bottom position
+		this.left = 1; // Left position
+		this.right = (columns - 2); // Right position
+		this.value = value; // Number of tiles within piece
+		this.grid = grid; // Actual piece as nested array
+
+		this.mesh = new THREE.Mesh(); // The 3D mesh associated with the piece
+		this.isPlaced = false; // If piece is currently on or off the board
+	}
+}
+
+// Class for player 
+export class Player { 
+	constructor(name, color) { // Takes two arguments (name, color)
+		this.name = name; // Player name
+		// Blokus pieces
+		// Grid goes down and right
+		// 1 = Tile
+		// 0 = Space beside a tile
+		// -1 = Corner tile
+		// -2 = Non connected tile
+		this.pieces = [
+			new Piece(3, 3, 1, 1, 1, [[-1, 0, -1], [0, 1, 0], [-1, 0, -1]]),
+			new Piece(3, 4, 1, 1, 2, [[-1, 0, 0, -1], [0, 1, 1, 0], [-1, 0, 0, -1]]),
+			new Piece(4, 4, 1, 2, 3, [[-1, 0, 0, -1], [0, 1, 1, 0], [-1, 0, 1, 0], [-2, -1, 0, -1]]),
+			new Piece(3, 5, 1, 2, 3, [[-1, 0, 0, 0, -1], [0, 1, 1, 1, 0], [-1, 0, 0, 0, -1]]),
+			new Piece(4, 4, 2, 1, 4, [[-1, 0, 0, -1], [0, 1, 1, 0], [0, 1, 1, 0], [-1, 0, 0, -1]]),
+			new Piece(4, 5, 2, 2, 4, [[-2, -1, 0, -1, -2], [-1, 0, 1, 0, -1], [0, 1, 1, 1, 0], [-1, 0, 0, 0, -1]]),
+			new Piece(3, 6, 1, 2, 4, [[-1, 0, 0, 0, 0, -1], [0, 1, 1, 1, 1, 0], [-1, 0, 0, 0, 0, -1]]),
+			new Piece(4, 5, 2, 3, 4, [[-2, -2, -1, 0, -1], [-1, 0, 0, 1, 0], [0, 1, 1, 1, 0], [-1, 0, 0, 0, -1]]),
+			new Piece(4, 5, 2, 2, 4, [[-2, -1, 0, 0, -1], [-1, 0, 1, 1, 0], [0, 1, 1, 0, -1], [-1, 0, 0, -1, -2]]),
+			new Piece(4, 6, 2, 1, 5, [[-1, 0, -1, -2, -2, -2], [0, 1, 0, 0, 0, -1], [0, 1, 1, 1, 1, 0], [-1, 0, 0, 0, 0, -1]]),
+			new Piece(5, 5, 3, 2, 5, [[-2, -1, 0, -1, -2], [-2, 0, 1, 0, -2], [-1, 0, 1, 0, -1], [0, 1, 1, 1, 0], [-1, 0, 0, 0, -1]]),
+			new Piece(5, 5, 3, 1, 5, [[-1, 0, -1, -2, -2], [0, 1, 0, -2, -2], [0, 1, 0, 0, -1], [0, 1, 1, 1, 0], [-1, 0, 0, 0, -1]]),
+			new Piece(4, 6, 1, 2, 5, [[-2, -1, 0, 0, 0, -1], [-1, 0, 1, 1, 1, 0], [0, 1, 1, 0, 0, -1], [-1, 0, 0, -1, -2, -2]]),
+			new Piece(5, 5, 2, 2, 5, [[-2, -2, -1, 0, -1], [-1, 0, 0, 1, 0], [0, 1, 1, 1, 0], [0, 1, 0, 0, -1], [-1, 0, -1, -2, -2]]),
+			new Piece(7, 3, 3, 1, 5, [[-1, 0, -1], [0, 1, 0], [0, 1, 0], [0, 1, 0], [0, 1, 0], [0, 1, 0], [-1, 0, -1]]),
+			new Piece(5, 4, 2, 1, 5, [[-1, 0, -1, -2], [0, 1, 0, -1], [0, 1, 1, 0], [0, 1, 1, 0], [-1, 0, 0, -1]]),
+			new Piece(5, 5, 2, 2, 5, [[-2, -1, 0, 0, -1], [-1, 0, 1, 1, 0], [0, 1, 1, 0, -1], [0, 1, 0, -1, -2], [-1, 0, -1, -2, -2]]),
+			new Piece(5, 4, 2, 1, 5, [[-1, 0, 0, -1], [0, 1, 1, 0], [0, 1, 0, -1], [0, 1, 1, 0], [-1, 0, 0, -1]]),
+			new Piece(5, 5, 2, 2, 5, [[-2, -1, 0, 0, -1], [-1, 0, 1, 1, 0], [0, 1, 1, 0, -1], [-1, 0, 1, 0, -2], [-2, -1, 0, -1, -2]]),
+			new Piece(5, 5, 2, 2, 5, [[-2, -1, 0, -1, -2], [-1, 0, 1, 0, -1], [0, 1, 1, 1, 0], [-1, 0, 1, 0, -1], [-2, -1, 0, -1, -2]]),
+			new Piece(4, 6, 2, 2, 5, [[-2, -1, 0, -1, -2, -2], [-1, 0, 1, 0, 0, -1], [0, 1, 1, 1, 1, 0], [-1, 0, 0, 0, 0, -1]]),
+		]
+		this.color = color; // Color of player's tiles
+		this.score = 0; // Player score
+		this.continue = true; // Forfeit status
+	}
+}
+
+// Function to check wether piece placement is in bounds
+function in_bounds(a_piece, row, column, grid) {
+	state = true; // State to be returned false if any of the tests fail
+	if ((row - (a_piece.index_point[0] - a_piece.top)) < 0) { // If the piece is not greater than 0 (rows)
+		state = false;
+	}
+	if (19 < (row + (a_piece.bottom - a_piece.index_point[0]))) { // If the piece is not greater than 0 (columns)
+		state = false;
+	}
+	if ((column - (a_piece.index_point[1] - a_piece.left)) < 0) { // If the piece is not less than or equal to 19 (rows)
+		state = false;
+	}
+	if (19 < (column + (a_piece.right - a_piece.index_point[1]))) { // If the piece is not less than or equal to 19 (columns)
+		state = false;
+	}
+	return state;
+}
+
+// Function to place the first piece in a corner of the grid
+function first_piece(a_piece, row, column, grid) {
+	let filled_corner = false; // Boolean to check if corner piece touches corner and has tile value 1
+	if (in_bounds(a_piece, row, column, grid)) { // If the piece is in bounds
+		if ((a_piece.index_point[0] - a_piece.top) == row) { 
+			if ((a_piece.index_point[1] - a_piece.left) == column) { // If the piece is in the top left corner
+				if (a_piece.grid[a_piece.top][a_piece.left] == 1) {
+					filled_corner = true;
+				}
+			}
+			else if ((a_piece.right - a_piece.index_point[1]) == (19 - column)) { // If the piece is in the bottom left corner
+				if (a_piece.grid[a_piece.top][a_piece.right] == 1) {
+					filled_corner = true;
+				}
+			}
+		}
+		else if ((a_piece.bottom - a_piece.index_point[0]) == (19 - row)) { // If the piece is in the top right corner
+			if ((a_piece.index_point[1] - a_piece.left) == column) {
+				if (a_piece.grid[a_piece.bottom][a_piece.left] == 1) {
+					filled_corner = true;
+				}
+			}
+			else if ((a_piece.right - a_piece.index_point[1]) == (19 - column)) { // If the piece is in the bottom right corner
+				if (a_piece.grid[a_piece.bottom][a_piece.right] == 1) {
+					filled_corner = true;
+				}
+			}
+		}
+	}
+	return filled_corner;
+}
+
+// Function to build the first pieces
+function place_piece(a_piece, row, column, grid, player) {
+	let row_start = row - a_piece.index_point[0]; // Distance from piece index and top of piece
+	let column_start = column - a_piece.index_point[1]; // Distance from piece index and left side of piece
+	for (let i = 0; i < a_piece.rows; i++) {
+		for (let j = 0; j < a_piece.columns; j++) {
+			if (a_piece.grid[i][j] == 1) { // If tile of piece is a filled tile
+				grid[row_start + i][column_start + j] = player.color; // Replace grid 0 with letter of player color
+			}
+		}
+	}
+}
+
+// Function to check if the area is a valid area to put a piece
+function area_check(a_piece, row, column, grid, player) {
+	let row_start = row - a_piece.index_point[0]; // Distance from piece index and top of piece
+	let column_start = column - a_piece.index_point[1]; // Distance from piece index and left side of piece
+	let not_touching = true; // Boolean if piece is touching another piece on its side of the same color 
+	let is_corner = false; // Boolean if piece's corner is touching another piece's corner of the same color
+	let is_zero = true; // Boolean if the piece is not covering an occupied tile of the grid
+	for (let i = 0; i < a_piece.rows; i++) {
+		for (j = 0; j < a_piece.columns; j++) {
+			if ((a_piece.grid[i][j] == 0) && (grid[row_start + i][column_start + j] == player.color)) {
+				not_touching = false;
+			}
+			if ((a_piece.grid[i][j] == -1) && (grid[row_start + i][column_start + j] == player.color)) {
+				is_corner = true;
+			}
+			if ((a_piece.grid[i][j] == 1) && (grid[row_start + i][column_start + j] != 0)) {
+				is_zero = false;
+			}
+		}
+	}
+	return ((is_corner) && (is_zero) && (not_touching));
+}
+
+// Main function for game
+function blokkus() {
+	let grid = []; //Create 20 by 20 grid
+	let row = 0;
+	let column = 0;
+	let player_list = []
+	let piece_num = 0;
+	let pass = true;
+	let game_state = true
+	for (let i = 0; i < 20; i++) {
+		grid[i] = [];
+		for (let j = 0; j < 20; j++) {
+			grid[i][j] = 0;
+		}
+	}
+	let num_players = window.prompt("How many players are there?: "); //Get the number of players
+	if (num_players == 2) {	
+		let player_1 = new Player("B"); // Creater player 1
+		let player_2 = new Player("R"); // Creater player 2
+		player_1.pieces = player_1.pieces.concat(player_1.pieces); // Add extra pieces (since only 2 players)
+		player_2.pieces = player_2.pieces.concat(player_2.pieces); // Add extra pieces (since only 2 players)
+		player_list = [player_1, player_2]; // Create player list
+		game_state = (player_1.continue || player_2.continue); // Create boolean for game state
+	}
+	else if (num_players == 3) {
+		let player_1 = new Player("B"); // Creater player 1
+		let player_2 = new Player("R"); // Creater player 2
+		let player_3 = new Player("Y"); // Creater player 3
+		player_list = [player_1, player_2, player_3]; // Create player list
+		game_state = (player_1.continue || player_2.continue || player_3.continue); // Create boolean for game state
+	}
+	else if (num_players == 4) {
+		let player_1 = new Player("B"); // Creater player 1
+		let player_2 = new Player("R"); // Creater player 2
+		let player_3 = new Player("Y"); // Creater player 3
+		let player_4 = new Player("G"); // Creater player 4
+		player_list = [player_1, player_2, player_3, player_4]; // Create player list
+		game_state = (player_1.continue || player_2.continue || player_3.continue || player_4.continue); // Create boolean for game state
+	}
+	for (let i = 0; i < player_list.length; i++) { // For loop for each player to place their first piece
+		do {
+			row = parseInt(window.prompt("Player " + i.toString() + ": Row [0 - 19]: "), 10);
+			column = parseInt(window.prompt("Player " + i.toString() + ": Column [0 - 19]: "), 10);
+			piece_num = window.prompt("Which piece number you want to put: ");
+			pass = first_piece(player_list[i].pieces[piece_num - 1], row, column, grid);
+			if (pass == false) {
+				console.log("Invalid first piece, please try again.");
+			}
+		}
+		while (pass == false);
+		place_piece(player_list[i].pieces[piece_num - 1], row, column, grid, player_list[i]);
+		player_list[i].pieces[piece_num - 1] = 0;
+		console.log(grid);
+		console.log(player_list[i].pieces);
+		pass = true;
+	}
+	do {
+		for (let i = 0; i < player_list.length; i++) {
+			if (player_list[i].continue == true) {
+				turn_input = window.prompt("Player " + i.toString() + " please make a decision (place a piece: 1 or concede: 2)");
+				if (turn_input == 1) {
+					do {
+						row = parseInt(window.prompt("Player " + i.toString() + ": Row [0 - 19]: "), 10);
+						column = parseInt(window.prompt("Player " + i.toString() + ": Column [0 - 19]: "), 10);
+						piece_num = window.prompt("Which piece number you want to put: ");
+						pass = area_check(player_list[i].pieces[piece_num - 1], row, column, grid, player_list[i]);
+						if (pass == false) {
+							console.log("Invalid piece placement, please try again.");
+						}
+					}
+					while (pass == false);
+					place_piece(player_list[i].pieces[piece_num - 1], row, column, grid, player_list[i]);
+					player_list[i].pieces[piece_num - 1] = 0;
+					console.log(grid);
+					console.log(player_list[i].pieces);
+					pass = true;
+				}
+				else {
+					player_list[i].continue = false;
+				}
+			}
+		}
+	}
+	while (game_state == true);
+}
--- blokkus/resources/js/board.js
+++ blokkus/resources/js/board.js
@@ -1,15 +1,16 @@
+import * as BLOKKUS from './blokkus.js'
 
 // ===========
 //  CONSTANTS
 // ===========
 
-const RIM_THICKNESS = 0.1
-const THICKNESS = 0.5
 const WHITE = 0xaaaaaa
 const RED = 0x880011
 const GREEN = 0x117722
 const BLUE = 0x003388
 const YELLOW = 0x999900
+const VALID_COLOR = 0x779977
+const INVALID_COLOR = 0x777777
 
 
 // ==================
@@ -19,7 +20,7 @@ const YELLOW = 0x999900
 // Scene, camera, and renderer
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000)
-camera.position.set(0,15,15)
+camera.position.set(0,13,16)
 const renderer = new THREE.WebGLRenderer({ antialias: true })
 
 // Camera cameraControls
@@ -27,6 +28,7 @@ const clock = new THREE.Clock()
 CameraControls.install( { THREE: THREE } );
 const cameraControls = new CameraControls(camera, renderer.domElement)
 cameraControls.enabled = false
+cameraControls.setTarget(0,-15,0)
 $(document).keydown(function(e){
 	if(e.keyCode == 32){
 		cameraControls.enabled = true
@@ -40,11 +42,37 @@ $(document).keyup(function(e){
 
 // Used to track the angles of the camera (azimuthal is side-to-side and polar is up-down)
 var currentAzimuthalAngle = 0
-var currentPolarAngle = Math.PI/4
+var currentPolarAngle = Math.PI/6
 
-// GLTF loader (for 3D models)
+// GLTF loader (for importing 3D models)
 const loader = new THREE.GLTFLoader()
 loader.setPath('resources/static/')
+
+// For raycasting
+const raycaster = new THREE.Raycaster()
+
+// For storing the mouse position on a scale from -1 to +1
+var mouse = new THREE.Vector2()
+
+// For storing the board
+var boardMesh
+
+// A 2-dimensional array for storing the game board studs (20x20)
+var studs = []
+
+// For storing the stud that is currently being hovered over
+var currentStud
+
+// For storing the currently selected piece
+var currentPiece
+
+// Array from storing the players
+var players = []
+
+// For storing the current player
+var currentPlayerIndex = 0
+var currentPlayer
+
 
 // ===========
 //  FUNCTIONS
@@ -81,41 +109,201 @@ function showAxis() {
 }
 
 // Moves the camera an angle of 90 degrees to the right
-function rotate() {
+window.rotate = function rotate() {
 	cameraControls.dampingFactor = 0.02
 	cameraControls.rotateTo(currentAzimuthalAngle+=Math.PI/2, currentPolarAngle, true)
 }
 
 // Moves the camera to the top (bird's eye view)
-function viewFromTop() {
+window.viewFromTop = function() {
 	cameraControls.dampingFactor = 0.1
 	cameraControls.rotateTo(currentAzimuthalAngle, currentPolarAngle=0, true)
 }
 
 // Moves the camera to an angle of 45 degrees to the hroizontal
-function viewFromSide() {
+window.viewFromSide = function() {
 	cameraControls.dampingFactor = 0.1
-	cameraControls.rotateTo(currentAzimuthalAngle, currentPolarAngle=Math.PI/4, true)
+	cameraControls.rotateTo(currentAzimuthalAngle, currentPolarAngle=Math.PI/6, true)
 }
 
-function createPieceMesh() {
-	let mesh
-	return new Promise(function(resolve, reject) {	
-		loader.load('tile.glb', function(gltf) {
+function addPlayers() {
+	let urlp = parseURL()
+	// If 2 players
+	if (Object.keys(urlp).length == 2) {
+		players.push(new BLOKKUS.Player(urlp['Player+Blue'], BLUE))
+		layPlayerPieces(players[0], 0)
+		players.push(new BLOKKUS.Player(urlp['Player+Red'], RED))
+		layPlayerPieces(players[1], Math.PI)
+	}
+	// If 3 players
+	else {
+		players.push(new BLOKKUS.Player(urlp['Player+Blue'], BLUE))
+		layPlayerPieces(players[0], 0)
+		players.push(new BLOKKUS.Player(urlp['Player+Red'], RED))
+		layPlayerPieces(players[1], Math.PI/2)
+		players.push(new BLOKKUS.Player(urlp['Player+Green'], GREEN))
+		layPlayerPieces(players[2], Math.PI)
+	}	
+	// If 4 players
+	if (Object.keys(urlp).length == 4) {
+		players.push(new BLOKKUS.Player(urlp['Player+Yellow'], YELLOW))
+		layPlayerPieces(players[3], 3*Math.PI/2)
+	}
+}
+
+function loadGLTF(filename){	
+	return new Promise( (resolve, reject) => {	
+		loader.load(filename, (gltf) => {
 			gltf.scene.traverse( (child) => {
 				if (child.type == 'Mesh') {
-					mesh = child
-					resolve()
+					resolve(child)
 				}
 			})
 		})
-	}).then(function() {
-		let material = new THREE.MeshPhongMaterial({ color:YELLOW })
-		mesh.material = material
-		mesh.position.set(0.5,0,0.5)
-		return mesh
 	})
 }
+
+function createPieceMesh(piece, color) {
+	let material = new THREE.MeshPhongMaterial({ color:color , shininess:50 })
+	material.transparent = true
+	material.opacity = 0.95
+
+	return loadGLTF('tile.glb').then(function(mesh) {
+		let pieceGeometry = new THREE.Geometry()
+		for (const [i, row] of piece.grid.entries()) {
+			for (const [j, tile] of row.entries()) {
+				if (tile == 1) {
+					let posX = i - piece.anchor_point[0]
+					let posZ = j - piece.anchor_point[1]
+
+					let tileGeometry = new THREE.Geometry().fromBufferGeometry(mesh.clone().geometry)
+					tileGeometry.translate(posX,0,posZ)
+					pieceGeometry.merge(tileGeometry)
+				}
+			}
+		}
+		return new THREE.Mesh(pieceGeometry, material)
+	})
+}
+
+// Lays each piece is front of the player (beware: long, hard-coded function)
+// Takes a rotation (which side of the board to put it on) - either 0, PI/2, PI, or 3PI/2
+function layPlayerPieces(player, rotation) {
+
+	let rotatedMatrix = new THREE.Matrix4().makeRotationY(rotation)
+
+	createPieceMesh(player.pieces[0], player.color).then( (mesh) => {
+		player.pieces[0].mesh = mesh
+		player.pieces[0].mesh.position.set(-10,-0.5,11).applyMatrix4(rotatedMatrix)
+		player.pieces[0].mesh.rotation.y = rotation
+		scene.add(player.pieces[0].mesh)
+	})
+	createPieceMesh(player.pieces[1], player.color).then( (mesh) => {
+		player.pieces[1].mesh = mesh
+		player.pieces[1].mesh.position.set(-8,-0.5,11).applyMatrix4(rotatedMatrix)
+		player.pieces[1].mesh.rotation.y = rotation
+		scene.add(player.pieces[1].mesh)
+	})
+}
+
+// TODO: Implement function that checks if valid position
+function isValidPosition() {
+	return true
+}
+
+// Casts a shadow of the currentPiece on the studs
+// References the global variable mouseCoordinates
+function castPieceShadow() {
+
+	let color = INVALID_COLOR
+	if(isValidPosition()) {
+		color = VALID_COLOR
+		$('body').css('cursor', 'pointer')
+	}
+
+	for (const [i, row] of currentPiece.grid.entries()) {
+		for (const [k, stud] of row.entries()) {
+
+			let posX = i + 10 + currentStud.position.x - currentPiece.anchor_point[0]
+			let posZ = k + 10 + currentStud.position.z - currentPiece.anchor_point[1]
+
+			if (stud == 1) {
+				try {
+					studs[posX][posZ].material.color.set(color)
+				}
+				catch(e){}
+			}
+		}
+	}
+}
+
+function placePiece() {
+	currentPiece.mesh.position.set(currentStud.position.x, 2, currentStud.position.z)
+	dropPieceAnimation(currentPiece, 0)
+	currentPiece.isPlaced = true
+}
+
+function raisePieceAnimation(piece, height) {
+	let mesh = piece.mesh
+	mesh.position.y = (height+0.01)-(((height+0.01)-mesh.position.y)*0.95)
+	if (mesh.position.y < height)
+		requestAnimationFrame( () => {raisePieceAnimation(piece, height)})
+}
+
+function dropPieceAnimation(piece, height) {
+	let mesh = piece.mesh
+	mesh.position.y = height+((mesh.position.y-height)*0.95)-0.001
+	if (mesh.position.y > height)
+		requestAnimationFrame( () => {dropPieceAnimation(piece, height)})
+}
+
+
+// ===========
+//  LISTENERS
+// ===========
+
+$(document).mousemove(function() {
+	// Calculates and stores mouse position on a scale from -1 to +1
+	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+})
+
+$(document).click(function() {
+	// For placing a piece
+	if (isValidPosition() && currentPiece && currentStud) {
+		placePiece()
+		currentPiece = null
+		setTimeout( () => {
+			viewFromSide()
+			rotate()
+		}, 1500)
+		currentPlayerIndex = ++currentPlayerIndex % 4
+		currentPlayer = players[currentPlayerIndex]
+	}
+
+	// For selecting/deselecting a piece
+	// Raycast the current player's pieces
+	for (const piece of currentPlayer.pieces) {
+		let intersected = raycaster.intersectObject(piece.mesh)[0]
+		// Raycasting found no object or object is already placed
+		if (!intersected || piece.isPlaced) {}
+		else {
+			if (currentPiece) {
+				var p = currentPiece
+				dropPieceAnimation(p, -0.5)
+			}
+			currentPiece = piece
+			raisePieceAnimation(currentPiece, 1)
+			break
+		}
+	}
+
+	// For clicking on board (not the studs)
+	if (raycaster.intersectObject(boardMesh)[0]) {} // Do nothing
+
+	
+})
+
 
 // ================
 //	DOCUMENT READY
@@ -135,8 +323,6 @@ var pointLight = new THREE.PointLight(0xffffff, 0.5)
 pointLight.position.set(0,20,0)
 scene.add(pointLight)
 
-showAxis()
-
 // Game board
 var board = new THREE.Shape([
 	new THREE.Vector2(-10,10),
@@ -148,43 +334,75 @@ var boardExtrudeSettings = {steps:1, depth:0.5 , bevelEnabled:false}
 var boardGeometry = new THREE.ExtrudeGeometry(board, boardExtrudeSettings)
 boardGeometry.rotateX(Math.PI/2)
 
-// Studs
-var stud = new THREE.Shape([
-	new THREE.Vector2(RIM_THICKNESS, RIM_THICKNESS),
-	new THREE.Vector2(RIM_THICKNESS, 1 - RIM_THICKNESS),
-	new THREE.Vector2(1 - RIM_THICKNESS, 1 - RIM_THICKNESS),
-	new THREE.Vector2(1 - RIM_THICKNESS, RIM_THICKNESS)
-	])
-var studExtrudeSettings = {
-	steps:1, 
-	depth:0, 
-	bevelEnabled:true, 
-	bevelThickness: 0.1,
-	bevelSize:0.1,
-	bevelOffset:-0.1,
-	bevelSegments:1
-}
-for(let i = -10; i < 10; i++) {
-	for(let j = -10; j < 10; j++) {
-		let studGeometry = new THREE.ExtrudeGeometry(stud, studExtrudeSettings)
-		studGeometry.rotateX(Math.PI/2).translate(i,0,j)
-		let studMesh = new THREE.Mesh(studGeometry)
-		// Add each stud to the geometry of the board
-		boardGeometry.merge(studMesh.geometry, studMesh.matrix)
-	}
-}
-
 // Create the board mesh
 var boardMaterial = new THREE.MeshStandardMaterial({ color: WHITE })
-var boardMesh = new THREE.Mesh(boardGeometry, boardMaterial)
+boardMesh = new THREE.Mesh(boardGeometry, boardMaterial)
 scene.add(boardMesh)
+
+// Studs
+loadGLTF('stud.glb').then((mesh) => {
+	let material = new THREE.MeshStandardMaterial({ color: WHITE })
+	for (let i = -10; i < 10; i++) {
+		let row = []
+		for (let j = -10; j < 10; j++) {
+			let m = mesh.clone()
+			m.material = material.clone()
+			m.position.set(i,0,j)
+			scene.add(m)
+			row.push(m)
+		}
+		studs.push(row)
+	}
+})
+
+// Add players and players' pieces
+addPlayers()
+currentPlayer = players[currentPlayerIndex]
+
 
 // Render or animate loop
 function animate() {
 	requestAnimationFrame(animate)
 
+	// Camera control and positioning
 	const delta = clock.getDelta()
 	cameraControls.update(delta)
+
+	// Set every stud to white
+	currentStud = null
+	for (const row of studs) {
+		for (const stud of row) {
+			stud.material.color.set(WHITE)
+		}
+	}
+
+	// Reset mouse cursor type
+	$('body').css('cursor', 'default')
+
+	// Raycasting
+	raycaster.setFromCamera(mouse,camera)
+
+	// Raycast studs
+	for (const row of studs) {
+		// intersectObjects returns an array of intersected objects
+		let intersected = raycaster.intersectObjects(row)[0]
+		// Raycasting found no objects
+		if (!intersected) {} 
+		// Only display piece shadow if a piece is selected
+		else if (currentPiece) {
+			currentStud = intersected.object
+			castPieceShadow()
+		}
+	}
+
+	// Raycast the current player's pieces
+	for (const piece of currentPlayer.pieces) {
+		let intersected = raycaster.intersectObject(piece.mesh)[0]
+		// Raycasting found no object or object is already placed
+		if (!intersected || piece.isPlaced) {}
+		else
+			$('body').css('cursor', 'pointer')
+	}
 
 	renderer.render(scene,camera)
 }
